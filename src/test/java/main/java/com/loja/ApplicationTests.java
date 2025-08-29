eae9c49a2fdb4ce4ed430e835e2df7e5b88cd784
vitoriarafaelly <vitoria.rafaelly98@gmail.com>
2025-08-29 00:48:10 -0300

    primeiro commit

---
+++ src/test/java/main/java/com/loja/ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.java.com.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
